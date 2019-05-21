7430523b3e932a8684c894c08931cae153669125
Panagiotis Astithas <pastith@gmail.com>
2019-05-20 21:59:27 -0700

    Optimize webcompat & criticals queries and first cut at duplicates query

--- bugs.js
+++ bugs.js
@@ -41,8 +41,9 @@ const fetchBugs = async (listFile = 'data/list.csv', keyFile = 'api-key.ini') =>
         const website = line.split(',')[1];
         bugzilla.push(await getBugzilla(website, bugzillaKey));
         duplicates.push(await getDuplicates(website, bugzillaKey, githubKey));
-        webcompat.push(await getWebcompat(website, githubKey));
-        criticals.push(await getCriticals(website, githubKey));
+        const { webcompatResult, criticalsResult } = await getWebcompat(website, githubKey);
+        webcompat.push(webcompatResult);
+        criticals.push(criticalsResult);
         console.log(`Fetched bug data for website ${website}`);
     }
     return bugTable;
@@ -73,7 +74,7 @@ const getKeys = async (keyFile) => {
  * @param {*} bugzillaKey
  */
 const getBugzilla = async (website, bugzillaKey) => {
-    const query = `https://bugzilla.mozilla.org/buglist.cgi?f1=OP&bug_file_loc_type=allwordssubstr&o3=greaterthan&list_id=14636479&v3=2018&resolution=---&bug_file_loc=${website}&query_format=advanced&f3=creation_ts&bug_status=UNCONFIRMED&bug_status=NEW&bug_status=ASSIGNED&bug_status=REOPENED&product=Core&product=Fenix&product=Firefox%20for%20Android&product=Firefox%20for%20Echo%20Show&product=Firefox%20for%20FireTV&product=Firefox%20for%20iOS&product=GeckoView&product=Web%20Compatibility&keywords_type=nowords&keywords=meta%2C%20&status_whiteboard_type=notregexp&status_whiteboard=sci%5C-exclude&api_key=${bugzillaKey}`;
+    const query = `https://bugzilla.mozilla.org/buglist.cgi?f1=OP&bug_file_loc_type=allwordssubstr&o3=greaterthan&list_id=14636479&v3=2018&resolution=---&bug_file_loc=${website}&query_format=advanced&f3=creation_ts&bug_status=UNCONFIRMED&bug_status=NEW&bug_status=ASSIGNED&bug_status=REOPENED&product=Core&product=Fenix&product=Firefox%20for%20Android&product=Firefox%20for%20Echo%20Show&product=Firefox%20for%20FireTV&product=Firefox%20for%20iOS&product=GeckoView&product=Web%20Compatibility&keywords_type=nowords&keywords=meta%2C%20&status_whiteboard_type=notregexp&status_whiteboard=sci%5C-exclude`;
     const apiQuery = `https://bugzilla.mozilla.org/rest/bug?include_fields=id,summary,status&bug_file_loc=${website}&bug_file_loc_type=allwordssubstr&bug_status=UNCONFIRMED&bug_status=NEW&bug_status=ASSIGNED&bug_status=REOPENED&f1=OP&f3=creation_ts&keywords=meta%2C%20&keywords_type=nowords&o3=greaterthan&product=Core&product=Fenix&product=Firefox%20for%20Android&product=Firefox%20for%20Echo%20Show&product=Firefox%20for%20FireTV&product=Firefox%20for%20iOS&product=GeckoView&product=Web%20Compatibility&resolution=---&status_whiteboard=sci%5C-exclude&status_whiteboard_type=notregexp&v3=2018&api_key=${bugzillaKey}`;
     const results = await fetch(apiQuery)
         .then(res => {
@@ -87,16 +88,21 @@ const getBugzilla = async (website, bugzillaKey) => {
 }
 
 /**
- * Returns open webcompat bugs that have the `engine-gecko` label.
+ * Returns open webcompat bugs that have the `engine-gecko` label and
+ * severity-critical webcompat bugs.
  * That should be:
  *     browser - firefox, browser - firefox - mobile,
  *     browser - firefox - tablet, browser - fenix,
  *     browser - focus - geckoview, browser - firefox - reality
  * @param {*} website
  * @param {*} githubKey
+ * @returns an Object with a webcompatResult and a criticalsResult properties
+ *          that correspond to each query
  */
 const getWebcompat = async (website, githubKey) => {
-    const query = `https://github.com/search?p=8&q=${website}+in%3Atitle+repo%3Awebcompat%2Fweb-bugs%2F+state%3Aopen+label:engine-gecko&type=Issues`;
+    const spaced = website.replace(/\./g, " ");
+    const webcompatQuery = `https://github.com/search?q=${spaced}+in%3Atitle+repo%3Awebcompat%2Fweb-bugs%2F+state%3Aopen+label:engine-gecko&type=Issues`;
+    const criticalsQuery = `https://github.com/webcompat/web-bugs/issues?q=${spaced}+in%3Atitle+repo%3Awebcompat%2Fweb-bugs%2F+is%3Aopen+label%3Aseverity-critical+label:engine-gecko`;
     const octokit = new Octokit({
         auth: `token ${githubKey}`,
         userAgent: 'past/tsci',
@@ -122,46 +128,15 @@ const getWebcompat = async (website, githubKey) => {
         state: "open",
         labels: "engine-gecko"
     });
-    const count = results.data.filter(element => !element.pull_request)
-        .filter(element => element.title.includes(website)).length;
-    return `=HYPERLINK("${query}"; ${count})`;
-}
-
-/**
- * Return severity-critical webcompat bugs.
- * @param {*} website
- * @param {*} githubKey
- */
-const getCriticals = async (website, githubKey) => {
-    const query = `https://github.com/webcompat/web-bugs/issues?q=${website}+in%3Atitle+repo%3Awebcompat%2Fweb-bugs%2F+is%3Aopen+label%3Aseverity-critical+label:engine-gecko`;
-    const octokit = new Octokit({
-        auth: `token ${githubKey}`,
-        userAgent: 'past/tsci',
-        throttle: {
-            onRateLimit: (retryAfter, options) => {
-                octokit.log.warn(`Request quota exhausted for request to ${options.url}`)
-                console.log(`retry count: ${options.request.retryCount}`);
-                if (options.request.retryCount === 0) { // only retries once
-                    console.log(`Retrying after ${retryAfter} seconds!`)
-                    return true
-                }
-                return false;
-            },
-            onAbuseLimit: (retryAfter, options) => {
-                // does not retry, only logs a warning
-                octokit.log.warn(`Abuse detected for request to ${options.url}`)
-            }
-        }
-    });
-    const results = await octokit.issues.listForRepo({
-        owner: "webcompat",
-        repo: "web-bugs",
-        state: "open",
-        labels: "engine-gecko,severity-critical"
-    });
-    const count = results.data.filter(element => !element.pull_request)
-        .filter(element => element.title.includes(website)).length;
-    return `=HYPERLINK("${query}"; ${count})`;
+    const geckoResults = results.data.filter(element => !element.pull_request)
+        .filter(element => element.title.includes(website));
+    const webcompatCount = geckoResults.length;
+    const criticals = geckoResults.filter(element => element.labels.includes("severity-critical"));
+    const criticalsCount = criticals.length;
+    return {
+        webcompatResult: `=HYPERLINK("${webcompatQuery}"; ${webcompatCount})`,
+        criticalsResult: `=HYPERLINK("${criticalsQuery}"; ${criticalsCount})`
+    };
 }
 
 /**
@@ -181,7 +156,107 @@ const getCriticals = async (website, githubKey) => {
  * @param {*} githubKey
  */
 const getDuplicates = async (website, bugzillaKey, githubKey) => {
-    return 0;
+    const apiQuery = `https://bugzilla.mozilla.org/rest/bug?include_fields=id,see_also&f1=see_also&f2=bug_status&f3=bug_file_loc&o1=anywordssubstr&o2=anywordssubstr&o3=casesubstring&v1=webcompat.com%2Cgithub.com%2Fwebcompat&v2=UNCONFIRMED%2CNEW%2CASSIGNED%2CREOPENED&v3=${website}&limit=0&api_key=${bugzillaKey}`
+    const results = await fetch(apiQuery)
+        .then(res => {
+            if (!res.ok) {
+                console.log(util.inspect(res, { showHidden: false, depth: null }))
+                throw new Error("Bugzilla query failed!");
+            }
+            return res.json();
+        });
+
+    const githubCandidates = [];
+    const regex = /\/(\d+)$/;
+    for (const bug of results.bugs) {
+        const bzId = bug.id;
+        for (const seeAlsoLink of bug.see_also) {
+            if (seeAlsoLink.includes("webcompat.com") ||
+                seeAlsoLink.includes("github.com/webcompat")) {
+                    const matches = regex.exec(seeAlsoLink);
+                    if (matches) {
+                        const githubId = matches[matches.length - 1];
+                        githubCandidates.push([githubId, bzId]);
+                    }
+                }
+        }
+    }
+    // GitHub search queries (q parameter) cannot be too long, so do >1 requests.
+    const searches = [];
+    const baseSearchQuery = "is%3Aissue+milestone%3Aduplicate+repo%3Awebcompat%2Fweb-bugs%2F";
+    let searchQuery = baseSearchQuery;
+    let searchMapGhToBz = new Map();
+    let i = 0;
+    while (i < githubCandidates.length) {
+        const [ githubId, bzId ] = githubCandidates[i];
+        i++;
+        if (searchQuery.length + 1 + githubId.length > 256) {
+            searches.push([searchQuery, searchMapGhToBz]);
+            searchQuery = baseSearchQuery;
+            searchMapGhToBz = new Map();
+        }
+        searchQuery += "+" + githubId;
+        searchMapGhToBz.set([parseInt(githubId)], bzId);
+    }
+    searches.push([searchQuery, searchMapGhToBz]);
+
+    const dupedBzIds = new Set();
+    const octokit = new Octokit({
+        auth: `token ${githubKey}`,
+        userAgent: 'past/tsci',
+        throttle: {
+            onRateLimit: (retryAfter, options) => {
+                octokit.log.warn(`Request quota exhausted for request to ${options.url}`)
+                console.log(`retry count: ${options.request.retryCount}`);
+                if (options.request.retryCount === 0) { // only retries once
+                    console.log(`Retrying after ${retryAfter} seconds!`)
+                    return true
+                }
+                return false;
+            },
+            onAbuseLimit: (retryAfter, options) => {
+                // does not retry, only logs a warning
+                octokit.log.warn(`Abuse detected for request to ${options.url}`)
+            }
+        }
+    });
+    for (const [query, ghToBzMap] of searches) {
+        const milestoneSearch = `https://api.github.com/search/issues?per_page=100&q=${query}`;
+        // TODO use Octokit
+        // const results = await octokit.issues.listForRepo({
+        //     owner: "webcompat",
+        //     repo: "web-bugs",
+        //     state: "open",
+        //     labels: "engine-gecko"
+        // });
+        const results = await fetch(milestoneSearch)
+            .then(res => {
+                if (!res.ok) {
+                    console.log(util.inspect(res, { showHidden: false, depth: null }))
+                    throw new Error("Bugzilla query failed!");
+                }
+                return res.json();
+            });
+        if (results.incomplete_results) {
+            throw new Error("Should not have over 100 results for just ${ids.length} search items"); // TODO figure out ${ids.length}
+        }
+        for (const item of results.items) {
+            const bzId = ghToBzMap.get(item.number);
+            if (bzId && item.milestone.title === "duplicate") {
+                dupedBzIds.add(bzId);
+            }
+        }
+    }
+
+    if (dupedBzIds.size === 0) {
+        return 0;
+    }
+    let param = "";
+    for (const id of dupedBzIds) {
+        param += "%2C" + id;
+    }
+    const bzLink = `https://bugzilla.mozilla.org/buglist.cgi?o1=anyexact&v1=${param}&f1=bug_id`;
+    return `=HYPERLINK("${bzLink}"; ${dupedBzIds.size})`;
 }
 
 module.exports = {
