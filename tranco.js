0569c66b29c4b2535e04b3a83ca87ee8db8d40ca
Panagiotis Astithas <pastith@gmail.com>
2019-05-24 16:26:48 -0700

    Fetch the Tranco list of the correct cut-off date. Fixes #15

--- tranco.js
+++ tranco.js
@@ -1,28 +1,25 @@
 const fs = require('fs');
 const fetch = require('node-fetch');
 
-const fetchList = async (size = 500, file = "data/list.csv") => {
+const fetchList = async (size = 500, directory = "data/", date) => {
     const LATEST_LIST_URL = 'https://tranco-list.eu/top-1m-id';
-
-    // Check for an already downloaded list.
-    const listIsCached = await fs.promises.access(file, fs.constants.R_OK | fs.constants.W_OK)
-        .then(() => true)
-        .catch(() => false);
-    if (listIsCached) {
-        console.log("Found cached Tranco list");
-        return;
-    }
 
     // Create the data directory.
     await new Promise((resolve, reject) => {
-        fs.mkdir('data', { recursive: true }, (err) => {
+        fs.mkdir(directory, { recursive: true }, (err) => {
             if (err) reject(err);
             resolve();
         });
     });
 
-    // Fetch the latest list ID.
-    const LIST_ID = await fetch(LATEST_LIST_URL)
+    // Fetch the requested list ID.
+    let ID_URL = LATEST_LIST_URL;
+    if (date) {
+        ID_URL = `https://tranco-list.eu/daily_list_id?date=${parseDate(date)}`;
+    } else {
+        date = new Date();
+    }
+    const LIST_ID = await fetch(ID_URL)
         .then(res => {
             if (!res.ok ||
                 res.headers.get('content-type') !== 'text/plain; charset=utf-8') {
@@ -30,8 +27,18 @@ const fetchList = async (size = 500, file = "data/list.csv") => {
             }
             return res.text();
         });
+    const file = `${directory}list-${parseDate(date)}.csv`;
 
-    // Fetch the latest list.
+    // Check for an already downloaded list.
+    const listIsCached = await fs.promises.access(file, fs.constants.R_OK | fs.constants.W_OK)
+        .then(() => true)
+        .catch(() => false);
+    if (listIsCached) {
+        console.log("Found cached Tranco list");
+        return file;
+    }
+
+    // Fetch the list.
     const LIST_URL = `https://tranco-list.eu/download/${LIST_ID}/${size}`;
     await fetch(LIST_URL)
         .then(res => {
@@ -43,11 +50,21 @@ const fetchList = async (size = 500, file = "data/list.csv") => {
                 const dest = fs.createWriteStream(file);
                 res.body.pipe(dest);
                 dest.on('finish', () => {
-                    console.log(`Downloaded latest Tranco list, ID: ${LIST_ID}`);
+                    console.log(`Downloaded Tranco list with ID ${LIST_ID} for date ${parseDate(date)}`);
                     resolve();
                 });
             });
         });
+    return file;
+}
+
+/**
+ * Return the specified date formatted as yyyy-mm-dd in UTC.
+ * @param {Date} date
+ * @returns a String representation of the date as yyyy-mm-dd
+ */
+function parseDate(date) {
+    return date.toISOString().split("T")[0];
 }
 
 module.exports = {
