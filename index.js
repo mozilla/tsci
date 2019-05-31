4ed9514cc182d682424497e2a2ab0c36d57f7c0e
Panagiotis Astithas <pastith@gmail.com>
2019-05-31 13:57:27 -0700

    Allow maxDate to be a year or a month

--- index.js
+++ index.js
@@ -3,6 +3,17 @@ const tranco = require('./tranco');
 const spreadsheet = require('./spreadsheet');
 const bugs = require('./bugs');
 
+/**
+ * Return the End of the week the specified date belongs to.
+ * @param {Date} date the date for which to
+ * @returns the date of the end of the week
+ */
+function getEOW(date) {
+    const param = new Date(date);
+    param.setDate(param.getDate() - param.getDay() + 7);
+    return new Date(param - 1);
+}
+
 const main = async () => {
     const config = require('./config.json');
     const LIST_SIZE = config.listSize || 500;
@@ -10,35 +21,60 @@ const main = async () => {
     const bugzillaKey = config.bugzillaKey || '';
     const githubKey = config.githubKey || '';
     const writers = config.writers || ['user@example.com'];
-    let maxDate = config.maxDate || null;
+    const queryDates = [];
+    const maxDate = config.maxDate || null;
 
-    const week = process.argv[2] || maxDate;
-    if (week) {
+    const inputDate = process.argv[2] || maxDate;
+    if (inputDate) {
         // We want to consider open bugs only until the end of the given week.
-        const parsed = new Date(week);
+        const parsed = new Date(inputDate);
         if (isNaN(parsed)) {
             throw new Error("Wrong date format: use yyyy-mm-dd");
         }
-        const weekday = parsed.getDay();
-        parsed.setDate(parsed.getDate() - weekday + 7);
-        maxDate = new Date(parsed - 1);
+
+        if (!inputDate.includes("-")) {
+            // An entire year is specified.
+            for (let i = 0; i < 52; i++) {
+                queryDates.push(getEOW(parsed));
+                parsed.setDate(parsed.getDate() + 7);
+            }
+        } else if (inputDate.indexOf("-") === inputDate.lastIndexOf("-")) {
+            // An entire month is specified.
+            for (let i = 0; i < 4; i++) {
+                queryDates.push(getEOW(parsed));
+                parsed.setDate(parsed.getDate() + 7);
+            }
+        } else {
+            queryDates.push(getEOW(parsed));
+        }
+    } else {
+        queryDates.push(inputDate);
     }
 
-    const LIST_FILE = await tranco.fetchList(LIST_SIZE, LIST_DIR, maxDate);
-    const bugTable = await bugs.fetchBugs(LIST_FILE, bugzillaKey, githubKey, undefined, maxDate);
+    for (const date of queryDates) {
+        const LIST_FILE = await tranco.fetchList(LIST_SIZE, LIST_DIR, date);
+        const bugTable = await bugs.fetchBugs(LIST_FILE, bugzillaKey, githubKey, undefined, date);
+        if (bugTable.get("bugzilla").length +
+            bugTable.get("webcompat").length +
+            bugTable.get("criticals").length +
+            bugTable.get("duplicates").length === 0) {
+                console.warn("List was empty or malformed!");
+                continue;
+        }
 
-    const SCOPES = ['https://www.googleapis.com/auth/drive.file'];
-    const auth = await google.auth.getClient({ scopes: SCOPES });
-    const sheets = google.sheets({ version: 'v4', auth });
-    const drive = google.drive({ version: 'v3', auth })
+        const SCOPES = ['https://www.googleapis.com/auth/drive.file'];
+        const auth = await google.auth.getClient({ scopes: SCOPES });
+        const sheets = google.sheets({ version: 'v4', auth });
+        const drive = google.drive({ version: 'v3', auth })
 
-    const title = 'Top Site Compatibility Index';
-    const id = await spreadsheet.createSpreadsheet(drive, title, LIST_FILE);
-    await spreadsheet.addStaticData(sheets, id, LIST_SIZE, maxDate);
-    await spreadsheet.addBugData(sheets, id, bugTable);
-    // TODO add a graph
-    for (const writer of writers) {
-        await spreadsheet.shareSheet(drive, id, writer);
+        const title = 'Top Site Compatibility Index';
+        const id = await spreadsheet.createSpreadsheet(drive, title, LIST_FILE);
+        await spreadsheet.addStaticData(sheets, id, LIST_SIZE, date);
+        await spreadsheet.addBugData(sheets, id, bugTable);
+        // TODO add a graph
+        for (const writer of writers) {
+            await spreadsheet.shareSheet(drive, id, writer);
+        }
     }
 }
 
